51ca39c824b3da34c77b1e40812094a362e157b7
taniadr <taniadiasr@gmail.com>
2020-01-15 14:16:35 -0300

    found constraint - utf-8 encoding need

--- api.py
+++ api.py
@@ -8,28 +8,32 @@ books = [
     {'id': 0,
      'title': 'A Fire Upon the Deep',
      'author': 'Vernor Vinge',
-     #'first_sentence': 'The coldsleep itself was dreamless.',
      'year_published': '1992'},
     {'id': 1,
      'title': 'The Ones Who Walk Away From Omelas',
      'author': 'Ursula K. Le Guin',
-     #'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
      'published': '1973'},
     {'id': 2,
      'title': 'Dhalgren',
      'author': 'Samuel R. Delany',
-     #'first_sentence': 'to wound the autumnal city.',
      'published': '1975'},
      {'id': 3,
      'title': 'Zen Para Distraidos',
      'author': 'Monja',
-     #'first_sentence': 'retorne ao darma...',
      'published': '2017'},
      {'id': 4,
      'title': 'A Monja e o Professor',
      'author': 'Monja',
-     #'first_sentence': 'retorne ao darma...',
-     'published': '2019'}
+     'published': '2018'},
+     {'id': 5,
+     'title': 'Aprenda a Viver o Agora',
+     'author': 'Monja Coen',
+     'published': '2019'},
+     {'id': 6,
+     'title': 'A Sabedoria da Transformação',
+     'author': 'Monja Coen',
+     'published': '2014'}
+
 ]
 
 
